0d06541b8f85f3bdffa83d7ef0cb5d4fb5a666bc
Samuel Souza <61950221+SamuelSouza2020@users.noreply.github.com>
2022-03-14 11:43:06 -0300

    Layout

--- Connect/Form1.Designer.cs
+++ Connect/Form1.Designer.cs
@@ -45,7 +45,6 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
-            this.pictureBox1.Image = global::Desafio5.Properties.Resources.logo3;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(268, 89);
